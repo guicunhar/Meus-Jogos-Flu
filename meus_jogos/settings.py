6107ee53ae256fad8c8dbe8df7a33340ad093a24
Guilherme Cunha <guicunhar@outlook.com>
2025-02-25 18:46:38 -0300

    Update DATABASES on Settings

--- meus_jogos/settings.py
+++ meus_jogos/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,11 +66,11 @@ WSGI_APPLICATION = 'meus_jogos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'
+    )
 }
+
 
 
 # Password validation
